dca4049a212d8bc53209561195fe59f2aad4ce80
Beatrizfleon <Beatrizfleon@gmail.com>
2020-03-20 18:22:27 +0100

    added placeholder images

--- js/main.js
+++ js/main.js
@@ -17,14 +17,31 @@ function getApiData(ev) {
       paintSeries();
     });
 }
+
+// function getSeriesHtmlCode() {
+//   let htmlCode = '';
+//   htmlCode += `<article class="serie-item">`;
+//   htmlCode += `<img src="http://static.tvmaze.com/uploads/images/medium_portrait/0/2400.jpg" alt=" />`;
+//   htmlCode += `<h2 class="title">Breaking Bad</h2>`;
+//   htmlCode += `</article>`;
+//   return htmlCode;
+// }
 function paintSeries() {
   let htmlCode = '';
-  htmlCode += `<ul>`;
+
   for (const serie of series) {
-    htmlCode += `<li>${serie.show.name}</li>`;
+    htmlCode += `<article class="serie-item">`;
+    if (serie.show.image != null) {
+      htmlCode += `<img src="${serie.show.image.medium}" alt=" />`;
+    } else {
+      htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?" alt=" />`;
+    }
+    htmlCode += `<h2 class="title">${serie.show.name}</h2>`;
+    htmlCode += `</article>`;
     console.log(serie.show.name);
   }
-  htmlCode += `</ul>`;
   seriesResult.innerHTML = htmlCode;
 }
 searchButton.addEventListener('click', getApiData);
+
+//Listen series
