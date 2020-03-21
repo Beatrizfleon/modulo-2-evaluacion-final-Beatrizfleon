09f0996385db4a4f6c9c09e92d4e6214c75f91f5
Beatrizfleon <Beatrizfleon@gmail.com>
2020-03-21 23:55:43 +0100

    added deletion of favorites

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 'use strict';
-console.log('hola mundo');
+// console.log('hola mundo');
 
 let series = [];
+let favourites = [];
 let searchInput = document.querySelector('.js-search');
 let searchButton = document.querySelector('.js-search-btn');
 let seriesResult = document.querySelector('.js-series');
+let addToFavourites = document.querySelector('.js-selected-series');
 
 function getApiData(ev) {
   ev.preventDefault();
@@ -12,7 +14,7 @@ function getApiData(ev) {
   fetch(`http://api.tvmaze.com/search/shows?q=${text}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      // console.log(data);
       series = data;
       paintSeries();
     });
@@ -26,22 +28,131 @@ function getApiData(ev) {
 //   htmlCode += `</article>`;
 //   return htmlCode;
 // }
+
 function paintSeries() {
   let htmlCode = '';
 
   for (const serie of series) {
-    htmlCode += `<article class="serie-item">`;
+    htmlCode += `<article class="js-add-serie serie-item" data-id="${serie.show.id}" >`;
     if (serie.show.image != null) {
-      htmlCode += `<img src="${serie.show.image.medium}" alt=" />`;
+      htmlCode += `<img src="${serie.show.image.medium}" data-id="${serie.show.id}" />`;
     } else {
-      htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?" alt=" />`;
+      htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?" data-id="${serie.show.id}"/>`;
     }
-    htmlCode += `<h2 class="title">${serie.show.name}</h2>`;
+    htmlCode += `<h2 class="title" data-id="${serie.show.id}">${serie.show.name}</h2>`;
     htmlCode += `</article>`;
-    console.log(serie.show.name);
+    // console.log(serie.show.name);
   }
   seriesResult.innerHTML = htmlCode;
+
+  listenAddSeries();
 }
 searchButton.addEventListener('click', getApiData);
 
 //Listen series
+function listenAddSeries() {
+  const seriesItems = document.querySelectorAll('.js-add-serie');
+  for (const seriesItem of seriesItems) {
+    // console.log(seriesItem);
+    seriesItem.addEventListener('click', addSerie);
+  }
+}
+
+function addSerie(ev) {
+  const clickedId = parseInt(ev.target.dataset.id);
+  let foundId;
+  for (const favourite of favourites) {
+    if (favourite.show.id === clickedId) {
+      foundId = favourite;
+    }
+  }
+  if (foundId === undefined) {
+    // busco la serie clickada en el array de series inicial
+
+    for (const serie of series) {
+      if (serie.show.id === clickedId) {
+        favourites.push(serie);
+      }
+    }
+  }
+  paintFavourites();
+}
+
+function removeFavourite(ev) {
+  const clickedId = parseInt(ev.target.dataset.id);
+
+  for (let index = 0; index < favourites.length; index += 1) {
+    if (favourites[index].show.id === clickedId) {
+      favourites.splice(index, 1);
+    }
+  }
+
+  paintFavourites();
+}
+
+function listenRemoveFavorite() {
+  const favouritesItems = document.querySelectorAll('.js-delete-cross');
+  for (const favouritesItem of favouritesItems) {
+    // console.log(favouritesItem);
+    favouritesItem.addEventListener('click', removeFavourite);
+  }
+}
+
+// function addSerie(ev) {
+//   const clickedId = ev.target.dataset.id;
+//   for (const serie of series) {
+//     if (clickedId === parseInt(serie.show.id)) {
+//       let exists = checkIfFavouriteExist(serie.show.id);
+//       if (exists === true) {
+//         // no hago nada
+//       } else {
+//         favourites.push(serie);
+//       }
+
+//       // if (checkIfFavouriteExist(serie.show.id)) { //el favorito ya existe y no lo añado
+//       // } else {
+//       //   favourites.push(serie);
+//       // }
+//     }
+//   }
+//   paintFavourites();
+// }
+
+// function checkIfFavouriteExist(idFavourite) {
+//   let found = false;
+
+//   for (const favourite of favourites) {
+//     if (idFavourite === favourite.show.id) {
+//       found = true;
+//     }
+//   }
+//   return found;
+// }
+
+// function checkIfFavouriteExist(id) {
+//   for (const favourite of favourites) {
+//     if (id === favourite.show.id) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+function paintFavourites() {
+  let htmlCodeFavourites = '';
+  for (const favourite of favourites) {
+    htmlCodeFavourites += `<article class="selected-item">`;
+    if (favourite.show.image != null) {
+      htmlCodeFavourites += `<img src="${favourite.show.image.medium}"  class="img-favourite" />`;
+    } else {
+      htmlCodeFavourites += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?" class="img-favourite"/>`;
+    }
+    htmlCodeFavourites += `<div class="title-icon">`;
+    htmlCodeFavourites += `<h2 class="favourite-title">${favourite.show.name}</h2>`;
+    htmlCodeFavourites += `<span><i class="fas fa-times-circle js-delete-cross" data-id="${favourite.show.id}"></i></span>`;
+    htmlCodeFavourites += `</div>`;
+    htmlCodeFavourites += `</article>`;
+  }
+  addToFavourites.innerHTML = htmlCodeFavourites;
+  listenRemoveFavorite();
+}
